6e78588105852ec59d8950fa1190536c37e64934
Wang, Chen <Chen.Wang@emc.com>
2016-08-04 15:31:44 +0800

    add separator ":" for image name and tag in url

--- registry/urls.py
+++ registry/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+REPO_PATTERN = '\w|\-|\d|/|_|.'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<name>[\w|-|\d]+)/$', views.repository, name='repository'),
-    url(r'^(?P<name>[\w|-|\d]+)/(?P<tag>[\w|-|\d|.]+)/$', views.image, name='image'),
+    url(r'^(?P<name>[' + REPO_PATTERN + ']+)/$', views.repository, name='repository'),
+    url(r'^(?P<name>[' + REPO_PATTERN + ']+)/:(?P<tag>[\w|\-|\d|.|/]+)/$', views.image, name='image'),
 ]
